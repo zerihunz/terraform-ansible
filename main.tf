92f804dbc88bc3612901680bfa02dff4e4a62b5b
zerihunz <zerihunz@gmail.com>
2021-11-16 17:23:25 -0500

    VPC and destroy ec2

--- main.tf
+++ main.tf
@@ -5,12 +5,14 @@ provider "aws" {
   secret_key = "D4w/ACsPRdsp6BmiqS2yD52m63oim8VG3vYrVlp2"
 }
 
-resource "aws_instance" "foobarweb" {
-  ami           = "ami-0747bdcabd34c712a"
-  instance_type = "t2.micro"  
+#resource "aws_instance" "foobarweb" {
+#  ami           = "ami-0747bdcabd34c712a"
+#  instance_type = "t2.micro"  
+#  tags = {
+#    Name = "Hello Foo"
+#  }
 
-  
-}
+#}
 
 
 
