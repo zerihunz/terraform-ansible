5439c143c2afaca1c4970ed37491fb25de035a9f
ztegegn <zerihun.tegegn@cgifederal.com>
2021-11-16 11:26:23 -0500

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAV3EUPNTH26URKMWB"
+  secret_key = "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"
+}
+
+resource "aws_instance" "foobarweb" {
+  ami           = "ami-0747bdcabd34c712a"
+  instance_type = "t2.micro"  
+
+  
+}
+
+
+
+#resource "<provider>_<resource_type>" "name" {
+#    config options.....connection 
+#    key = "value"  
+#    key2 = "another value"
+#} 
