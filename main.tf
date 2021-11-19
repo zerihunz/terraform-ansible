db68dff8edf17ac34f5b9c2649eba2be6780ed47
zerihunz <zerihunz@gmail.com>
2021-11-18 21:09:05 -0500

    wrap up terraform

--- main.tf
+++ main.tf
@@ -22,6 +22,12 @@ resource "aws_vpc" "prod-vpc" {
   }
 }
 
+variable "subnet_prefix" {
+  description = "cidr block for the subnet" 
+  #default     = "10.0.66.0/24"       //If no value is provided, the default here will be used.
+  #type = string
+}
+
 #2.Create Internet Gateway
 resource "aws_internet_gateway" "prod-gw" {
   vpc_id = aws_vpc.prod-vpc.id
@@ -42,7 +48,7 @@ resource "aws_route_table" "prod-route-table" {
 
   route {
     ipv6_cidr_block        = "::/0"
-    egress_only_gateway_id = aws_internet_gateway.prod-gw.id
+    gateway_id = aws_internet_gateway.prod-gw.id
   }
 
   tags = {
@@ -53,11 +59,25 @@ resource "aws_route_table" "prod-route-table" {
 #4.Create a Subnet where our webserver is going to reside on
 resource "aws_subnet" "subnet-1" {
   vpc_id     = aws_vpc.prod-vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.subnet_prefix[0]                 //variable defined above
+  #cidr_block = var.subnet_prefix[0].cidr_block
   availability_zone = "us-east-1a"
 
   tags = {
     Name = "prod-subnet"
+    #Name = var.subnet_prefix[0].name                with the object, use it this way
+  }
+}
+
+resource "aws_subnet" "subnet-2" {
+  vpc_id     = aws_vpc.prod-vpc.id
+  cidr_block = var.subnet_prefix[1]                 //variable defined above
+  #cidr_block = var.subnet_prefix[1].cidr_block
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "dev-subnet"
+    #Name = var.subnet_prefix[1].name
   }
 }
 
@@ -119,6 +139,50 @@ resource "aws_security_group" "prod_allow_web" {
 resource "aws_network_interface" "web-server-nic" {
   subnet_id       = aws_subnet.subnet-1.id
   private_ips     = ["10.0.1.50"]
-  security_groups = [aws_security_group.prod_allow_web.id]
-  
+  security_groups = [aws_security_group.prod_allow_web.id]  
+}
+
+output "server_public_ip" {         //This will show public ip everytime we run "terraform apply"
+  value = aws_eip.one.public_ip
+}
+
+#8.Assign an elastic IP to the network interface created in step 7 (Elastic IP is a public IP that's routable on the internet)
+resource "aws_eip" "one" {
+  vpc                       = true    // (Optional) Boolean if the EIP is in a VPC or not.
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.prod-gw]      //Can't create Elastic IP without gateway, so use depends on 
+}
+
+#9.Create ubuntu server and install/enable apache2
+
+resource "aws_instance" "web-server-instance" {
+  ami = "ami-0279c3b3186e54acd"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"       //Availability zone should be hardcoded this way other wise aws would complain it's in a different zone by assigning a random availability zone
+  key_name = "main-key"
+
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install apache2 -y
+              sudo systemctl start apache2
+              sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+              EOF
+  tags = {
+    Name = "web-server"
+  }              
+}
+
+output "server_private_ip" {
+  value = aws_instance.web-server-instance.private_ip
+}
+
+output "server_id" {
+  value = aws_instance.web-server-instance.id
 }
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+subnet_prefix = ["10.0.1.0/24", "10.0.2.0/24"]
+#also you can use an object here like below
+#subnet_prefix = [{ cidr_block = "10.0.1.0/24", name = "prod_subnet" }, { cidr_block = "10.0.2.0/24", name = "dev_subnet" }]
